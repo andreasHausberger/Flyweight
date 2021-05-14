785e5226a86e840209067f1cfaa50cc65de85c99
Andreas Hausberger <hausbergerandreas1@gmail.com>
2021-05-14 19:26:07 +0200

    Added Platform

--- Sources/Flyweight/Network.swift
+++ Sources/Flyweight/Network.swift
@@ -102,33 +102,25 @@ public class Network<T: Codable> {
         }
     }
     
+    /// Performs network call with a type-agnostic array as a body
     private static func performNetworkCall(urlString: String, method: Method, urlParameter params: [String:String]? = nil, httpHeaders headers: [String: String]? = nil, body: [String: Any]? = nil, log: LoggingStyle) throws -> AnyPublisher<T,APIError>  {
         guard var urlComponents = URLComponents(string: urlString) else {
             throw APIError.invalidURL
         }
         
         let config = getConfig()
-        
         let session = URLSession(configuration: config)
-        
         if let params = params {
-            urlComponents.queryItems = [URLQueryItem]()
-            params.forEach { (key, value) in
-                let item = URLQueryItem(name: key, value: value)
-                urlComponents.queryItems?.append(item)
-            }
+            setURLParameters(components: &urlComponents, params: params)
         }
-        
+
         guard let url = urlComponents.url else { throw APIError.invalidURL }
-        
         var request = URLRequest(url: url)
-        
         if let headers = headers {
             headers.forEach { (key, value) in
                 request.setValue(value, forHTTPHeaderField: key)
             }
         }
-        
         request.httpMethod = method.rawValue
         
         if let bodyObject = body {
@@ -137,7 +129,6 @@ public class Network<T: Codable> {
             }
             request.httpBody = bodyData
         }
-        
         
         return session.dataTaskPublisher(for: request)
             .tryMap { response in // tries to map the response (data) as a HTTPURLResponse, checks Status code.
@@ -151,17 +142,14 @@ public class Network<T: Codable> {
             .decode(type: T.self, decoder: JSONDecoder()) //decodes the response.data as JSON, and tries to map it to a Codable object.
             .mapError { APIError.map($0) } //if an error occurs during any of this, it gets mapped.
             .eraseToAnyPublisher() // all weird stuff is erased, a clean AnyPublisher is returned - just as we want.
-        
     }
     
     private static func log(style: LoggingStyle, request: URLRequest, response: HTTPURLResponse) {
         switch style {
         case .none:
             break
-            
         case .normal:
             let message = "\(request.httpMethod ?? ""): \(request.url?.description ?? ""), Status: \(response.statusCode)"
-            
             NSLog(message)
         }
     }
@@ -170,7 +158,55 @@ public class Network<T: Codable> {
         let config = URLSessionConfiguration.default
         config.requestCachePolicy = .reloadIgnoringLocalCacheData
         config.urlCache = nil
-        
         return config
+    }
+    
+    private static func setURLParameters(components: inout URLComponents, params: [String: String]) {
+        components.queryItems = [URLQueryItem]()
+        params.forEach { name, value in
+            components.queryItems?.append(URLQueryItem(name: name, value: value))
+        }
+    }
+}
+
+@available(OSX 10.15, *)
+@available(iOS 13.0, *)
+extension Network {
+
+    
+    private static func performNetworkCall<B: Codable>(urlString: String, method: Method, urlParameter params: [String:String]? = nil, httpHeaders headers: [String: String]? = nil, body: B? = nil, log: LoggingStyle) throws -> AnyPublisher<T,APIError>  {
+        guard var urlComponents = URLComponents(string: urlString) else { throw APIError.invalidURL }
+        let config = getConfig()
+        let session = URLSession(configuration: config)
+        if let params = params {
+            setURLParameters(components: &urlComponents, params: params)
+        }
+        
+        guard let url = urlComponents.url else { throw APIError.invalidURL }
+        var request = URLRequest(url: url)
+        if let headers = headers {
+            headers.forEach { (key, value) in
+                request.setValue(value, forHTTPHeaderField: key)
+            }
+        }
+        request.httpMethod = method.rawValue
+        
+        if let bodyObject = body {
+            guard let bodyData = try? JSONEncoder().encode(bodyObject) else { throw APIError.decoding }
+            request.httpBody = bodyData
+        }
+        
+        return session.dataTaskPublisher(for: request)
+            .tryMap { response in // tries to map the response (data) as a HTTPURLResponse, checks Status code.
+                guard let httpURLResponse = response.response as? HTTPURLResponse,
+                      (200...299).contains(httpURLResponse.statusCode) else {
+                    throw APIError.statusCode
+                }
+                Network.log(style: log, request: request, response: httpURLResponse)
+                return response.data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .mapError { APIError.map($0) }
+            .eraseToAnyPublisher()
     }
 }
